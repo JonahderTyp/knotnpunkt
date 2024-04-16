8d85fb55b1d73bb47f9ec949cbfae0abe78a0bcc
Jonah Wille <jonah.wille10@gmail.com>
2024-04-16 16:22:49 +0200

    feat(admin page): added auth

--- knotnpunkt/site/admin.py
+++ knotnpunkt/site/admin.py
@@ -1,9 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, abort
 from flask.templating import render_template
+from flask_login import current_user
 from flask_login.utils import login_required
+from ..database.db import Benutzer
 
 
 admin_site = Blueprint("admin_site", __name__, url_prefix="/admin")
+
+
+@admin_site.before_request
+@login_required
+def auth():
+    usr: Benutzer = current_user
+    if not usr.rechte().get("einstellungenLesen", False):
+        abort(401)
+        # return redirect(url_for('site.login')) # alternativ zum Fehlercode
+    pass
 
 
 @admin_site.route("/einstellungen")
